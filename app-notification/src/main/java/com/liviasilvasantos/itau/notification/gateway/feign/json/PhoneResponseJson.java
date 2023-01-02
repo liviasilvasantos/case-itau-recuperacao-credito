b2227e27c32a09fdd35cebfb98c9fcafe54e4859
Livia Silva Santos <liviasilvasantos@gmail.com>
2023-01-02 18:28:11 -0300

    fix notification

--- app-notification/src/main/java/com/liviasilvasantos/itau/notification/gateway/feign/json/PhoneResponseJson.java
+++ app-notification/src/main/java/com/liviasilvasantos/itau/notification/gateway/feign/json/PhoneResponseJson.java
@@ -3,11 +3,13 @@ package com.liviasilvasantos.itau.notification.gateway.feign.json;
 import com.liviasilvasantos.itau.notification.domain.customer.PhoneType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PhoneResponseJson implements Serializable {
